7137a8f38decf11304cdb30f5a3e115848caeceb
jcruz <jcruz@tre-pa.jus.br>
2019-05-16 13:46:44 -0300

    Ajuste no módulo

--- src/main/java/br/jus/tre_pa/jsecurity/JSecurityModuleConfiguration.java
+++ src/main/java/br/jus/tre_pa/jsecurity/JSecurityModuleConfiguration.java
@@ -1,12 +1,21 @@
 package br.jus.tre_pa.jsecurity;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @ComponentScan(basePackages = "br.jus.tre_pa.jsecurity")
 @AutoConfigurationPackage
 public class JSecurityModuleConfiguration {
 
+	@PostConstruct
+	private void init() {
+		log.info("JSecurityModuleConfiguration loaded.");
+	}
 }
