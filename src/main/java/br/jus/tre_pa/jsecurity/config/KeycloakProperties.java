4c5293d6c71325f8c4463b7cd9a6c92289e418fd
jcruz <jcruz@localhost>
2019-05-27 08:31:11 -0300

    Ajustes nas properties

--- src/main/java/br/jus/tre_pa/jsecurity/config/KeycloakProperties.java
+++ src/main/java/br/jus/tre_pa/jsecurity/config/KeycloakProperties.java
@@ -7,12 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Classe de Properties com as propriedades do keycloak. Além dos das
+ * propriedades padrões do keycloak (iniciadas com o prefixo 'keycloak.') é
+ * definido o prefixo 'kc' para propriedades customizadas.
+ * 
+ * @author jcruz
+ *
+ */
 @Configuration
 @ConfigurationProperties(prefix = "kc")
 @Getter
 @Setter
-public class KeycloakProperties  {
-	
+public class KeycloakProperties {
+
 	@Value("${keycloak.auth-server-url}")
 	private String authServerUrl;
 
@@ -24,7 +32,7 @@ public class KeycloakProperties  {
 
 	@Value("${keycloak.resource}")
 	private String clientId;
-	
+
 	private String baseUrl;
-	
+
 }
