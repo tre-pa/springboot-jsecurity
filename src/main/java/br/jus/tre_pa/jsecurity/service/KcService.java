aed8181c4a61cb67c44cf9cd803b28989001c699
jcruz <jcruz@tre-pa.jus.br>
2019-06-06 14:23:40 -0300

    Ajuste na classe de service

--- src/main/java/br/jus/tre_pa/jsecurity/service/KcService.java
+++ src/main/java/br/jus/tre_pa/jsecurity/service/KcService.java
@@ -14,51 +14,102 @@ import org.keycloak.representations.idm.authorization.ScopeRepresentation;
 import org.keycloak.representations.idm.authorization.TimePolicyRepresentation;
 import org.keycloak.representations.idm.authorization.UserPolicyRepresentation;
 
+/**
+ * Classe com os serviços par manipulação do Keycloak.
+ * 
+ * @author jcruz
+ *
+ */
 public interface KcService {
 
 	/**
 	 * Método registrador de Realm.
 	 * 
-	 * @param representation RealmRepresentation
+	 * @param representation {@link RealmRepresentation}
 	 */
 	void register(RealmRepresentation representation);
 
 	/**
 	 * Método registrador do Client.
 	 * 
-	 * @param representation ClientRepresentation
+	 * @param representation {@link ClientRepresentation}
 	 */
 	void register(ClientRepresentation representation);
 
 	/**
 	 * Método registrador do AuthzScope.
 	 * 
-	 * @param authScope
+	 * @param authScope ScopeRepresentation {@link ScopeRepresentation}
 	 */
 	void register(ScopeRepresentation representation);
-	
+
 	/**
 	 * Método registrador de Resource.
 	 * 
-	 * @param keycloakResource
+	 * @param keycloakResource {@link ResourceRepresentation}
 	 */
 	void register(ResourceRepresentation representation);
 
+	/**
+	 * Método registrador de Policy do tipo Aggregate.
+	 * 
+	 * @param representation {@link AggregatePolicyRepresentation}
+	 */
 	void register(AggregatePolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Policy do tipo Client.
+	 * 
+	 * @param representation {@link ClientPolicyRepresentation}
+	 */
 	void register(ClientPolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Policy do tipo Group.
+	 * 
+	 * @param representation {@link GroupPolicyRepresentation}
+	 */
 	void register(GroupPolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Policy do Tipo Javascript.
+	 * 
+	 * @param representation {@link JSPolicyRepresentation}
+	 */
 	void register(JSPolicyRepresentation representation);
 
-	void register(RolePolicyRepresentation representation );
+	/**
+	 * Método registrador de Policy Role.
+	 * 
+	 * @param representation {@link RolePolicyRepresentation}
+	 */
+	void register(RolePolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Policy Rule.
+	 * 
+	 * @param representation {@link RulePolicyRepresentation}
+	 */
 	void register(RulePolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Policy Time.
+	 * 
+	 * @param representation {@link TimePolicyRepresentation}
+	 */
 	void register(TimePolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Policy User.
+	 * 
+	 * @param representation {@link UserPolicyRepresentation}
+	 */
 	void register(UserPolicyRepresentation representation);
 
+	/**
+	 * Método registrador de Permission.
+	 * 
+	 * @param representation {@link ResourcePermissionRepresentation}
+	 */
 	void register(ResourcePermissionRepresentation representation);
 }
