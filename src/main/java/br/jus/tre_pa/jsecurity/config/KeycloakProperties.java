fc99e610c89c6d130392e68ba0f1ff36e0304750
jcruz <jcruz@localhost>
2019-07-19 14:27:51 -0300

    Ajustes no javadoc

--- src/main/java/br/jus/tre_pa/jsecurity/config/KeycloakProperties.java
+++ src/main/java/br/jus/tre_pa/jsecurity/config/KeycloakProperties.java
@@ -22,25 +22,49 @@ import lombok.Setter;
 @Setter
 public class KeycloakProperties {
 
+	/**
+	 * URL de conexão do keycloak.
+	 */
 	@Value("${keycloak.auth-server-url}")
 	private String authServerUrl;
 
+	/**
+	 * Realm padrão da aplicação.
+	 */
 	@Value("${keycloak.realm}")
 	private String realm;
 
+	/**
+	 * Secret da aplicação backend.
+	 */
 	@Value("${keycloak.credentials.secret}")
 	private String secret;
 
+	/**
+	 * ClientId da aplicação backend.
+	 */
 	@Value("${keycloak.resource}")
 	private String clientId;
 
+	/**
+	 * URL base da aplicação.
+	 */
 	private String baseUrl;
 
+	/**
+	 * URIs de redirecionamento da aplicação frontend.
+	 */
 	@Value("#{'${kc.redirect-uris}'.split(',')}")
 	private List<String> redirectUris;
 
+	/**
+	 * Usuário administrador do realm master do Keycloak.
+	 */
 	private String admUser;
 
+	/**
+	 * Password do usuário administrador do realm master do Keycloak.
+	 */
 	private String admPass;
 
 }
